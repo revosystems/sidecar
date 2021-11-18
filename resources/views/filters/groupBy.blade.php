35341eb4debd04c7c5618763df5550a97ce49d29
Jordi Puigdellívol <jordi.p@revo.works>
2021-11-18 10:49:42 +0100

    Show group by when available

--- resources/views/filters/groupBy.blade.php
+++ resources/views/filters/groupBy.blade.php
@@ -1,13 +1,17 @@
-<i class="fa fa-pie-chart fa-fw"></i>
-<select name="groupBy[]" multiple>
-    <option value="">--</option>
-    @foreach($report->availableGroupings() as $filter)
-        @foreach($filter->groupings() as $grouping)
-            <option value="{{$filter->getFilterField()}}:{{$grouping}}"
-                    @if ($report->filters->groupBy->isGroupingBy($filter->getFilterField(), $grouping)) selected @endif
-            >
-                {{ str_replace(" (default)", "", "{$filter->getTitle()} ({$grouping})") }}
-            </option>
-        @endforeach
-    @endforeach
-</select>
+@if ($report->availableGroupings()->count() > 0)
+    <div class="inline">
+        <i class="fa fa-pie-chart fa-fw"></i>
+        <select name="groupBy[]" multiple>
+            <option value="">--</option>
+            @foreach($report->availableGroupings() as $filter)
+                @foreach($filter->groupings() as $grouping)
+                    <option value="{{$filter->getFilterField()}}:{{$grouping}}"
+                            @if ($report->filters->groupBy->isGroupingBy($filter->getFilterField(), $grouping)) selected @endif
+                    >
+                        {{ str_replace(" (default)", "", "{$filter->getTitle()} ({$grouping})") }}
+                    </option>
+                @endforeach
+            @endforeach
+        </select>
+    </div>
+@endif
