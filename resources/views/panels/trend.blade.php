af26c4ec461e0e70acac4db11f1625ac81b05195
Jordi Puigdellívol <jordi.p@revo.works>
2021-11-24 13:49:28 +0100

    dashboard tooltip

--- resources/views/panels/trend.blade.php
+++ resources/views/panels/trend.blade.php
@@ -1,7 +1,7 @@
 <div class="sidecar-panel bg-white m-4 p-4 rounded shadow">
     <div class="flex justify-between font-bold">
         <div class='has-tooltip cursor'>
-            <span class='tooltip rounded shadow-lg p-2 text-xs bg-black text-white mt-5'> {{ $panel->tooltip }}</span>
+            <span class='tooltip rounded shadow-lg p-2 text-xs bg-black text-white mt-4'> {{ $panel->tooltip }}</span>
             <div class="" style="text-decoration:underline dotted">{!! __(config('sidecar.translationsPrefix').$panel->getTitle()) !!}</div>
         </div>
         <div class="text-xl"> {{ $last }}</div>
