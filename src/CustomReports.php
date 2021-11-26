1b438fa438c0ffa2587cf11c2f102451e7c4ea87
Jordi Puigdellívol <jordi.p@revo.works>
2021-11-26 10:37:35 +0100

    custom report with auth id

--- src/CustomReports.php
+++ src/CustomReports.php
@@ -11,23 +11,27 @@ class CustomReports
     static private $key = 'sidecar_savedReports';
 
     public static function all() : array {
-        return json_decode(Redis::get(static::$key), true) ?? [];
+        return json_decode(Redis::get(static::key()), true) ?? [];
     }
 
     public static function save(string $name, string $url)
     {
         $savedReports = static::all();
         $savedReports[] = ["name" => $name, "url" => $url];
-        Redis::set(static::$key, json_encode($savedReports));
+        Redis::set(static::key(), json_encode($savedReports));
     }
 
     public static function delete($name) {
         $savedReports = collect(static::all())->reject(function ($savedReport) use($name) {
             return $savedReport["name"] == $name;
         })->all();
-        Redis::set(static::$key, json_encode($savedReports));
+        Redis::set(static::key(), json_encode($savedReports));
     }
 
+    public static function key() : string
+    {
+        return collect([static::$key, auth()->id()])->implode("_");
+    }
 
 
 }
