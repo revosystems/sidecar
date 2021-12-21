8853c7df432180dac9d33c37bfb0032eec049d44
Jordi Puigdellívol <jordi.p@revo.works>
2021-12-21 10:49:32 +0100

    some things

--- resources/views/filters/number.blade.php
+++ resources/views/filters/number.blade.php
@@ -1,6 +1,6 @@
 @php $filterValues = $report->filters->filtersFor($field->getFilterField()) @endphp
 <select id="{{$field->getFilterField()}}-select" name="filters[{{$field->getFilterField()}}][operand]"
-        style="width: 55px; padding-top: 6.5px;">
+        style="width: 55px;">
     @foreach($field->filterOptions() as $operand => $name)
         <option value="{{$operand}}" @if($operand == data_get($filterValues, 'operand', '')) selected @endif> {{ $name }}</option>
     @endforeach
