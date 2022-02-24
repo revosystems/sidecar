eb025265218b1866e62d37bdf99f78d3d1a26b44
Jordi Puigdellívol <jordi.p@revo.works>
2022-02-24 12:59:29 +0100

    Improves date saving filter

--- src/ExportFields/Date.php
+++ src/ExportFields/Date.php
@@ -63,7 +63,7 @@ class Date extends ExportField
     {
         if (!$this->filterable) { return $query; }
         $businessRange = static::businessRange($values['start'], $values['end']);
-        session()->put('sidecar.date', ['start' => $values['start'], 'end' => $values['end'], 'period' => $values['period']]);
+        session()->put('sidecar.date', $values);
         if (!Str::contains($key, config('database.connections.mysql.prefix'))){
             $filters->applyDateFilter($query, $this->databaseTable().'.'.$key, $businessRange);
             return $filters->applyTimeFilter($query, $this->databaseTable().'.'.$key, $values);
