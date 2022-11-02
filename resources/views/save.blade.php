0ce077b4bb1a5d34028d339e4aa7947968657848
Adria Martin <adriamt1@gmail.com>
2022-11-02 10:33:34 +0100

    feature: show save report button only if report can be saved

--- resources/views/save.blade.php
+++ resources/views/save.blade.php
@@ -1,22 +1,24 @@
-<div class="float-right mt-4 mr-4" x-data="{open:false}">
-    <a class="button secondary relative" x-on:click="open=!open">
-        <i class="fa fa-clone" aria-hidden="true"></i>
-        {{ __(config('sidecar.translationsPrefix').'save') }}
-    </a>
+@if($report->canBeSaved)
+    <div class="float-right mt-4 mr-4" x-data="{open:false}">
+        <a class="button secondary relative" x-on:click="open=!open">
+            <i class="fa fa-clone" aria-hidden="true"></i>
+            {{ __(config('sidecar.translationsPrefix').'save') }}
+        </a>
 
-    <div class="dropdown bg-white absolute shadow-xl z-50 m-4 p-4 right-0" x-on:click.away="open=!open" x-show="open" x-transition x-cloak>
-        <div class="mb-4">
-        {{ __(config('sidecar.translationsPrefix').'saveReportTitle') }}
-        </div>
-        <form action="{{ route('sidecar.report.store') }}" method="post">
-            {{ csrf_field() }}
-            <input type="hidden" name="url" value="{{ request()->fullUrl() }}">
-            <input name="name" placeholder="{{ __(config('sidecar.translationsPrefix').'myReport') }}" required>
-            <br>
-            <div class="mb-2 text-gray-400 text-sm mt-1">
-                {{ __(config('sidecar.translationsPrefix').'saveReportDesc') }}
+        <div class="dropdown bg-white absolute shadow-xl z-50 m-4 p-4 right-0" x-on:click.away="open=!open" x-show="open" x-transition x-cloak>
+            <div class="mb-4">
+                {{ __(config('sidecar.translationsPrefix').'saveReportTitle') }}
             </div>
-            <button class="button primary mt-2">{{ __(config('sidecar.translationsPrefix').'save') }}</button>
-        </form>
+            <form action="{{ route('sidecar.report.store') }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="url" value="{{ request()->fullUrl() }}">
+                <input name="name" placeholder="{{ __(config('sidecar.translationsPrefix').'myReport') }}" required>
+                <br>
+                <div class="mb-2 text-gray-400 text-sm mt-1">
+                    {{ __(config('sidecar.translationsPrefix').'saveReportDesc') }}
+                </div>
+                <button class="button primary mt-2">{{ __(config('sidecar.translationsPrefix').'save') }}</button>
+            </form>
+        </div>
     </div>
-</div>
+@endif
