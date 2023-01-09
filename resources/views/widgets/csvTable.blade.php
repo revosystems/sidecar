6382fe13d27ce5888c07558e307189f7984f4b95
Adria Martin <adriamt1@gmail.com>
2023-01-09 12:29:43 +0100

    fix: escape text when exporting to csv.

--- resources/views/widgets/csvTable.blade.php
+++ resources/views/widgets/csvTable.blade.php
@@ -1,8 +1,8 @@
 @if($rows)
 {{ $fields->map->getTitle()->implode(";") }}
 @foreach($rows as $row)
-    {{ $fields->map(function($field) use($row) {
+    {!! $fields->map(function($field) use($row) {
         return $field->toCsv($row);
-    })->implode(";") }}
+    })->implode(";") !!}
 @endforeach
 @endif
