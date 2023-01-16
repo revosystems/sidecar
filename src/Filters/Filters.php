e03cfdd832bfaf19d7a6368c7efb30d92cade86c
Martí Sala Morral <marti.sala@revo.works>
2023-01-16 10:05:29 +0100

    Clean up filters without value

--- src/Filters/Filters.php
+++ src/Filters/Filters.php
@@ -247,7 +247,9 @@ class Filters
     {
         if (!$filters) { return $filters; }
         return collect($filters)->reject(function ($value, $key) {
-            return isset($value['operand']) && (!isset($value['value']) || $value['value'] == null);
+            return isset($value['operand'])
+                ? !isset($value['value']) || $value['value'] == null
+                : !isset($value[0]) || $value[0] == null;
         })->all();
     }
 
