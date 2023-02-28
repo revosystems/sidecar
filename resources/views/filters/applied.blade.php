4197fd833929d5f033f875ddb5d365bde27811e5
Jordi Puigdellívol <jordi.p@revo.works>
2023-02-28 18:23:14 +0100

    Applied filters with multidimension array

--- resources/views/filters/applied.blade.php
+++ resources/views/filters/applied.blade.php
@@ -1,6 +1,11 @@
 <div id="sidecar-applied-filters" class="mt-4">
     @foreach($report->availableFilters()->sort() as $field)
-        @foreach($field->filterOptions() as $key => $value)
+        @if (countArrayDimensions($field->filterOptions()) > 1)
+            @php ($options = collect($field->filterOptions())->mapWithKeys(fn($item) => $item))
+        @else
+            @php ($options = $field->filterOptions())
+        @endif
+        @foreach($options as $key => $value)
             @if($report->filters->isFilteringBy($field->getFilterField(), $key))
                 <div class="inline bg-gray-200 text-xs text-gray-600 rounded p-1 mr-1">
                 {{ $value }}
