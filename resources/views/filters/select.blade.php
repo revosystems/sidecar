1dc0d71e653a56e24cbfcd27e841fe13fa8e696b
Jordi Puigdellívol <jordi.p@revo.works>
2023-02-28 18:14:01 +0100

    Select handles multiarray

--- resources/views/filters/select.blade.php
+++ resources/views/filters/select.blade.php
@@ -1,8 +1,31 @@
+@php
+    if (! function_exists('countArrayDimensions')) {
+        function countArrayDimensions($array) : int
+        {
+            if (is_array(reset($array)))
+                $return = countArrayDimensions(reset($array)) + 1;
+            else
+                $return = 1;
+            return $return;
+        }
+    }
+@endphp
+
 <select id="{{$field->getFilterField()}}" name="filters[{{$field->getFilterField()}}][]" multiple style="width: 300px">
     <option value="">--</option>
-    @foreach($field->filterOptions($report->filters) as $key => $value)
-        <option value="{{$key}}" @if($report->filters->isFilteringBy($field->getFilterField(), $key)) selected @endif>{{$value}}</option>
-    @endforeach
+    @if (countArrayDimensions($field->filterOptions($report->filters)) > 1)
+        @foreach($field->filterOptions($report->filters) as $category => $categories)
+            <optgroup label="{{ $category }}">
+            @foreach($categories as $key => $value)
+                <option value="{{ $key }}" @if($report->filters->isFilteringBy($field->getFilterField(), $key)) selected @endif>{{$value}}</option>
+            @endforeach
+            </optgroup>
+        @endforeach
+    @else
+        @foreach($field->filterOptions($report->filters) as $key => $value)
+            <option value="{{$key}}" @if($report->filters->isFilteringBy($field->getFilterField(), $key)) selected @endif>{{$value}}</option>
+        @endforeach
+    @endif
 </select>
 
 @push(config('sidecar.scripts-stack'))
