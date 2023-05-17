2649be1186228190e4bc6607b82fa09c2e20b677
Jordi Puigdellívol <jordi.p@revo.works>
2023-05-17 12:58:31 +0200

    Updates link files so they can have an icon

--- src/ExportFields/Link.php
+++ src/ExportFields/Link.php
@@ -4,12 +4,24 @@ namespace Revo\Sidecar\ExportFields;
 
 class Link extends Text
 {
+    protected $linkIcon = null;
+
+    public function linkIcon($icon) : self
+    {
+        $this->linkIcon = $icon;
+        return $this;
+    }
+
     public function getLinkTitle($row) : string {
+        if ($icon = $this->linkIcon){
+            return "<i class='fa fa-{$icon} fa-fw'></i>";
+        }
         return $this->getValue($row);
     }
 
     public function toHtml($row) : string {
         if ($this->route == null) { return $this->getValue($row); }
-        return link_to_route($this->route, $this->getLinkTitle($row), $this->getValue($row), ['class' => $this->linkClasses]);
+        $link = route($this->route, $this->getValue($row));
+        return "<a href='{$link}' class='{$this->linkClasses}' style='color:gray;'>{$this->getLinkTitle($row)}</a>";
     }
 }
