3b411606d71408a77d8c3678ead852b9cf08a7a4
Jordi Fernandez <jordi.fernandez@revo.works>
2024-02-12 16:37:37 +0100

    New BusinessDateTime field

---
+++ src/ExportFields/BusinessDateTime.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Revo\Sidecar\ExportFields;
+
+use Revo\Sidecar\Filters\GroupBy;
+use Illuminate\Support\Facades\DB;
+
+class BusinessDateTime extends Date
+{
+    public function getNonGroupedValue($value) : string {
+        return $this->getCarbonDate($value)->isoFormat('L HH:mm');
+    }
+
+    /**
+     * @param  GroupBy|null  $groupBy
+     * @return string|null the select field (or array of select fields) to include in the query
+     */
+    public function getSelectField(?GroupBy $groupBy = null)
+    {
+        $timezone    = Date::$timezone;
+        $openingTime = Date::$openingTime;
+
+        if ($this->computed) {
+            return $this->getComputedSelectField($groupBy);
+        }
+
+        if ($groupBy?->isGrouping()) {            
+            if ($groupBy->isGroupingBy($this->dependsOnField, 'hour')) {
+                return DB::raw("GS_orders.{$this->dependsOnField} as opened");
+            }
+            if ($groupBy->isGroupingBy($this->dependsOnField)) {
+                return DB::raw("DATE(SUBTIME(CONVERT_TZ(GS_orders.{$this->dependsOnField}, 'UTC', '{$timezone}'), '{$openingTime}')) as opened");
+            }
+
+            return $this->onGroupingBy
+                ? "{$this->onGroupingBy}({$this->dependOnFieldFull()}) as ".collect(explode(".",
+                    $this->dependsOnField))->last()
+                : null;
+        }
+
+        return $this->dependOnFieldFull();
+    }
+}
