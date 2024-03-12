e3d7ce1901baf2fe6dcfdc00bb36f89a6f632d36
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-12 18:18:13 +0100

    Graph type selector

--- resources/views/graphs/graph.blade.php
+++ resources/views/graphs/graph.blade.php
@@ -1,6 +1,14 @@
+
+<div class="bg-neutral-200 border ml-4 rounded-lg inline-block">
+    <div class="flex items-center gap-2">
+    <a href="{{request()->fullUrl()}}&graph_type=pie"  @class(["py-2 px-3 rounded-md text-gray-700", "bg-white shadow" => $graph->getType() == 'pie']) >@icon(chart-pie)</a>
+    <a href="{{request()->fullUrl()}}&graph_type=bar"  @class(["py-2 px-3 rounded-md text-gray-700", "bg-white shadow" => $graph->getType() == 'bar']) >@icon(chart-simple)</a>
+    <a href="{{request()->fullUrl()}}&graph_type=line" @class(["py-2 px-3 rounded-md text-gray-700", "bg-white shadow" => $graph->getType() == 'line']) >@icon(chart-line)</a>
+    </div>
+</div>
+
 <div class="bg-white shadow-sm m-4 p-4 relative" @if (in_array($graph->getType(), ['pie', 'doughnut']))  style="height:50vh" @else style="height:30vh" @endif>
     <canvas id="chart"></canvas>
-    {{-- pie | line | pie | doughnut --}}
 </div>
 
 @push(config('sidecar.scripts-stack'))
