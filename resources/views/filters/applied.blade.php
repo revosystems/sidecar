1e2b0e091549a32ce5c42b41d6fcaaa29e7ac397
Eduard Altimiras <eduard@revo.works>
2024-10-17 15:44:57 +0200

    Only show margins when there are available fields

--- resources/views/filters/applied.blade.php
+++ resources/views/filters/applied.blade.php
@@ -1,8 +1,14 @@
-<div id="sidecar-applied-filters" class="mt-4 flex flex-wrap items-center gap-1">
-    @php($count = 0)
-    @foreach($report->availableFilters() as $field)
-        @if($report->filters->isFilteringBy($field->getFilterField()))
-            @php($count++)
+@php($fields = [])
+@foreach($report->availableFilters() as $field)
+    @php($filterField = $field->getFilterField())
+    @if($report->filters->isFilteringBy($filterField))
+        @php($fields[] = $field)
+    @endif
+@endforeach
+
+@if(count($fields) > 0)
+    <div id="sidecar-applied-filters" class="mt-4 flex flex-wrap items-center gap-1">
+        @foreach($fields as $field)
             @if ($field instanceof \Revo\Sidecar\ExportFields\Boolean)
                 @include('sidecar::filters.applied.boolean')
             @elseif ($field instanceof \Revo\Sidecar\ExportFields\Number)
@@ -14,13 +20,11 @@
             @else
                 @include('sidecar::filters.applied.select')
             @endif
-        @endif
-    @endforeach
-    @if($count > 0)
-            <a href="{{ url(request()->getPathInfo()) }}">
-                    <x-ui::tertiary-button icon="xmark">
-                        {{__(config('sidecar.translationsPrefix').'clearAll')}}
-                    </x-ui::tertiary-button>
-            </a>
-    @endif
-</div>
+        @endforeach
+        <a href="{{ url(request()->getPathInfo()) }}">
+            <x-ui::tertiary-button icon="xmark">
+                {{__(config('sidecar.translationsPrefix').'clearAll')}}
+            </x-ui::tertiary-button>
+        </a>
+    </div>
+@endif
